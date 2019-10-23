568493ef748adabfa6099429a6c0994794432fa0
1744952 <andersonsutil.off@gmail.com>
2019-10-23 11:07:41 -0300

    Utiliando Diretivas *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,17 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  
+
 })
 export class AppComponent {
   title = 'Anderson Sutil';
-  photos = []
+  photos = [{
+    url: 'https://i.pinimg.com/originals/05/85/59/0585598979733e17e5c3c9a6f0309cf9.jpg',
+    description: 'Desenho'
+  },
+  {
+    url: 'https://i.pinimg.com/564x/21/ed/dc/21eddc409004041f8b8dd6e01c310481.jpg',
+    description: 'Desenho2'
+  }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { PhotosModule } from './photos/photos.module';
 @NgModule({
   declarations: [
     AppComponent,
-    
+
   ],
   imports: [
     BrowserModule,
