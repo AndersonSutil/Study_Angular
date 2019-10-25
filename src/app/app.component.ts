760f76fbb5461105750b9ea824700e37f10cc549
1744952 <andersonsutil.off@gmail.com>
2019-10-25 09:53:27 -0300

    lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from './photos/photo/photo.service';
-import { Photo } from './photos/photo/photo';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -9,18 +7,6 @@ import { Photo } from './photos/photo/photo';
   styleUrls: ['./app.component.css']
 
 })
-export class AppComponent implements OnInit {
-  title = 'Anderson Sutil';
-  photos: Photo[] = []; //<--- Para fotos retornar um Array de Objetos de tipo Photos
-
-  constructor(private photoService: PhotoService) { }
-
-  ngOnInit(): void {
-
-    this.photoService
-      .listFromUser('flavio')
-      .subscribe(photos => {console.log(photos[0].description),
-        this.photos = photos
-      });
-  }
+export class AppComponent {
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PhotoListComponent,
 
   ],
   imports: [
     BrowserModule,
     PhotosModule,
-    HttpClientModule   //<--- Importando o Modulo do  HttpClient 
+    HttpClientModule,  //<--- Importando o Modulo do  HttpClient 
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
